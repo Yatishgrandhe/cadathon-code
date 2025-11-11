2a9115bdfd6952fd6611e4d01f94e10299a53b24
justjai333 <justjaistudios@gmail.com>
2025-11-11 18:58:47 -0500

    Create theforce_intake.java

---
+++ src/main/java/frc/robot/subsystems/theforce_intake.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+  private static final int INTAKE_ID = 9;
+
+  private static final double INTAKE_IN_VOLTS = 3.0;
+
+  private final TalonFX motor = new TalonFX(INTAKE_ID);
+
+  private final VoltageOut control = new VoltageOut(0);
+
+  private void setMotorPower(double volts) {
+    motor.setControl(control.withOutput(volts));
+  }
+
+  public Command moveIntakeIn() {
+    return startEnd(() -> setMotorPower(INTAKE_IN_VOLTS), () -> setMotorPower(0));
+  }
+  
+}
